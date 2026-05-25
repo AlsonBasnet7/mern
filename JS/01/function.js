65d0cca82eff820e2148b882062a88640db15ed0
AlsonBasnet07 <basnetalson121@gmail.com>
2026-05-25 14:55:57 +0545

    pathing of the files

--- JS/01/function.js
+++
@@ -1,6 +0,0 @@
-function add(a,b){
-    return a+b
-}
-var number1=Number(prompt('Enter the first number'))
-var number2=Number(prompt("Enter the second number"))
-document.write(add(number1,number2));
